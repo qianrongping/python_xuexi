5f88ea940faec4cf4646553134a76de94360ca23
qianrongping <18588221194@163.com>
2020-06-13 15:10:17 +0800

    1

---
+++ Excel插入数据库/插入库存报表/excel_插入库存报表.py
@@ -0,0 +1,47 @@
+import pymysql
+import xlrd
+import time
+
+t1 = time.time()
+print("开始了")
+
+db = pymysql.connect(host='8.129.186.89',
+                     port=3306,
+                     user='root',
+                     password='db7b6a8392355c76',
+                     database='wms',
+                     charset='utf8')
+cur = db.cursor()
+ex = xlrd.open_workbook("半成品.xlsx")
+sheet = ex.sheet_by_index(0)
+rows = sheet.nrows
+cols = sheet.ncols
+
+# 跳过第一行的标题,直接从第二行开始
+for row_num in range(1, rows):
+    data = tuple(sheet.row_values((row_num, 0)[0]))
+    warehouse_id, materiel_id, materiel_stock = data
+    # print(materiel_code, materiel_name, materiel_spec, materiel_type, use_company, purchase_price, category_id, procedure_id, composition_id, memo)
+    sql = """insert into
+                warehouse_detail(warehouse_id, materiel_id, materiel_stock)
+            value
+            ('%s','%s','%s')""" % (warehouse_id, materiel_id, materiel_stock)
+    try:
+        # 执行sql语句
+        cur.execute(sql)
+        # 提交数据到数据库
+        db.commit()
+        print(f"成功了", materiel_id)
+
+    except Exception as e:
+        # 对修改失败的数据进行撤销,表示回滚
+        db.rollback()
+        print(f"失败了", materiel_id)
+        print(e)
+
+# 关闭游标
+cur.close()
+# 关闭连接
+db.close()
+t2 = time.time()
+print(t2 - t1)
